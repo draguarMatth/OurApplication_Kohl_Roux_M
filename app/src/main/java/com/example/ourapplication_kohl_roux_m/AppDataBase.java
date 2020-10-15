df3d8850a21d71991523210cb1b77a616e71ee7f
Matth_HES <Matth_HES@users.noreply.github.com>
2020-10-15 15:40:19 +0200

    Mieux DB Room 1

--- app/src/main/java/com/example/ourapplication_kohl_roux_m/AppDataBase.java
+++ app/src/main/java/com/example/ourapplication_kohl_roux_m/AppDataBase.java
@@ -3,8 +3,10 @@ package com.example.ourapplication_kohl_roux_m;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Trajet.class}, version = 1)
+@Database(entities = {Trajet.class, Car.class}, version = 1)
+//@Database(entities = {Car.class}, version= 1)
 public abstract class AppDataBase extends RoomDatabase {
 
     public abstract DbTrajetDao trajetDao();
+    public abstract DbCarDao carDao();
 }
--- app/src/main/java/com/example/ourapplication_kohl_roux_m/Car.java
+++ app/src/main/java/com/example/ourapplication_kohl_roux_m/Car.java
@@ -2,8 +2,10 @@ package com.example.ourapplication_kohl_roux_m;
 
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
+import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+@Entity
 public class Car {
 
     @PrimaryKey(autoGenerate = true)
