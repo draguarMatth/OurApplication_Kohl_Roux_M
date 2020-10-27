685dc464184a4aeb4f728f00345fc18a80514feb
mathieu <mathieu.kohl@students.hevs.ch>
2020-10-27 08:13:06 +0100

    Adding list trajet & main app

--- app/src/main/java/com/example/ourapplication_kohl_roux_m/main_app.java
+++ app/src/main/java/com/example/ourapplication_kohl_roux_m/main_app.java
@@ -1,13 +1,27 @@
 package com.example.ourapplication_kohl_roux_m;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class main_app extends AppCompatActivity {
 
+    private Button button;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
+
+        button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_app.this, list_trajet.class);
+                startActivity(intent);
+            }
+        });
     }
 }
