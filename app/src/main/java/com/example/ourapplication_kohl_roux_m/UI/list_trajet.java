fc97a852e8945188b93c86200864b7efe3e5b921
mathieu <mathieu.kohl@students.hevs.ch>
2020-11-05 17:35:43 +0100

    Menu bar

--- app/src/main/java/com/example/ourapplication_kohl_roux_m/UI/list_trajet.java
+++ app/src/main/java/com/example/ourapplication_kohl_roux_m/UI/list_trajet.java
@@ -1,6 +1,10 @@
 package com.example.ourapplication_kohl_roux_m.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Contacts;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,5 +28,15 @@ public class list_trajet extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    Intent intent = new Intent(view.getContext(), stats_dernier_trajet.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ourapplication_kohl_roux_m/UI/stats_dernier_trajet.java
@@ -0,0 +1,16 @@
+package com.example.ourapplication_kohl_roux_m.UI;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ourapplication_kohl_roux_m.R;
+
+public class stats_dernier_trajet extends AppCompatActivity {
+
+    protected void onCreate(Bundle savecInstacneState){
+        super.onCreate(savecInstacneState);
+        setContentView(R.layout.activity_stats_dernier_trajet);
+    }
+}
