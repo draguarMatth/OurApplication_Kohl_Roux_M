4ea945724fc61a9b2f51bde69a71a0afd51a7057
Matth_HES <Matth_HES@users.noreply.github.com>
2020-11-08 11:11:56 +0100

    Accès db 2

--- app/src/main/java/com/example/ourapplication_kohl_roux_m/DbCarDao.java
+++ app/src/main/java/com/example/ourapplication_kohl_roux_m/DbCarDao.java
@@ -2,6 +2,7 @@ package com.example.ourapplication_kohl_roux_m;
 
 import android.database.sqlite.SQLiteConstraintException;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -13,11 +14,19 @@ import java.util.List;
 public interface DbCarDao {
 //    Car car = new Car();
 
-    @Query("SELECT * FROM car WHERE uid LIKE (:UID) LIMIT 1")
+    @Query("SELECT * FROM car WHERE uid LIKE (:id) LIMIT 1")
+    LiveData<Car> getById(int id);
+
+    @Query("SELECT * FROM car")
+    LiveData<List<Car>> getAll();
+
+/*    @Query("SELECT * FROM car WHERE uid LIKE (:UID) LIMIT 1")
     Car getById(int UID);
 
     @Query("SELECT * FROM car")
     List<Car> getAllCar();
+
+ */
 
     @Query("SELECT * FROM car WHERE uid IN (:carIds)")
     List<Car> loadAllByIds(int[] carIds);
--- app/src/main/java/com/example/ourapplication_kohl_roux_m/dbClass/Repository/TrajetRepository.java
+++ app/src/main/java/com/example/ourapplication_kohl_roux_m/dbClass/Repository/TrajetRepository.java
@@ -6,10 +6,6 @@ import androidx.lifecycle.LiveData;
 
 import com.example.ourapplication_kohl_roux_m.BaseApp;
 import com.example.ourapplication_kohl_roux_m.dbClass.Trajet;
-<<<<<<< HEAD
-import com.example.ourapplication_kohl_roux_m.dbClass.asynch.CreateTrajet;
-=======
->>>>>>> Accès db 1
 import com.example.ourapplication_kohl_roux_m.dbClass.asynch.DeleteTrajet;
 import com.example.ourapplication_kohl_roux_m.dbClass.asynch.UpdateTrajet;
 import com.example.ourapplication_kohl_roux_m.dbClass.pojo.TrajetByThisCar;
@@ -35,32 +31,19 @@ public class TrajetRepository {
         return instance;
     }
 
-<<<<<<< HEAD
-    public LiveData<List<Trajet>> getTrajetByDate(final String date,
-                                                                    Application application) {
-        return ((BaseApp) application).getDatabase().trajetDao().getByDate(date);
-    }
-
-    public LiveData<TrajetByThisCar> getTrajetByName(final String date,
-=======
     public LiveData<Trajet> getTrajet(final String date, Application application) {
         return ((BaseApp) application).getDatabase().trajetDao().getByDate(date);
     }
 
-    public LiveData<List<TrajetByThisCar>> getOtherTrajetByThisCar(final String date,
->>>>>>> Accès db 1
+    public LiveData<TrajetByThisCar> getTrajetByName(final String date,
                                                                    Application application) {
         return ((BaseApp) application).getDatabase().trajetDao().getByName(date);
     }
 
     public void insert(final Trajet trajet, OnAsyncEventListener callback,
                        Application application) {
-<<<<<<< HEAD
         new CreateTrajet(application, callback).execute(trajet);
     }
-=======
-trajet    }
->>>>>>> Accès db 1
 
     public void update(final Trajet trajet, OnAsyncEventListener callback,
                        Application application) {
