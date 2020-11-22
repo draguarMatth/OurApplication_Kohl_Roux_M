dab100919c682f4ca6e1bcdb236b34b9b95cc863
Matth_HES <Matth_HES@users.noreply.github.com>
2020-11-22 15:18:27 +0100

    modif post v3

---
+++ app/src/main/java/com/example/ourapplication_kohl_roux_m/ui/car/FragementChooseNewCar.java
@@ -0,0 +1,23 @@
+package com.example.ourapplication_kohl_roux_m.ui.car;
+
+import android.widget.Spinner;
+
+public class FragementChooseNewCar {
+    private static final String TAG = "TransactionActivity";
+
+    private Spinner spinnerTradeMark;
+    private Spinner spinnerModel;
+    private Spinner spinnerModel;
+    private Spinner spinnerModel;
+    private Spinner spinnerModel;
+
+
+    private SortedMap<ClientEntity, List<AccountEntity>> clientEntityMultimap;
+
+    private ListAdapter<AccountEntity> adapterFromAccount;
+    private ListAdapter<AccountEntity> adapterToAccount;
+
+    private AccountListViewModel viewModel;
+
+
+}
